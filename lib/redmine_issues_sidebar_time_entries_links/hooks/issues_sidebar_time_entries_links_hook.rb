cd6064e328d501f9fc5cd220ac369fc59d796667
symarys <symarys@gmail.com>
2015-06-29 14:41:08 +0200

    Adding timelog links to project overview

--- lib/redmine_issues_sidebar_time_entries_links/hooks/issues_sidebar_time_entries_links_hook.rb
+++ lib/redmine_issues_sidebar_time_entries_links/hooks/issues_sidebar_time_entries_links_hook.rb
@@ -2,6 +2,7 @@ module RedmineIssuesSidebarTimeEntriesLinks
   module Hooks
     class IssuesSidebarTimeEntriesLinksHook < Redmine::Hook::ViewListener
       render_on(:view_issues_sidebar_issues_bottom, :partial => 'issues_sidebar_time_entries_links/add_links', :layout => false)
+      render_on(:view_projects_show_right, :partial => 'issues_sidebar_time_entries_links/add_timelog_links_to_project_overview', :layout => false)
     end
   end
 end
